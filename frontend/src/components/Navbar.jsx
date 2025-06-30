138d7f742c43ad81de660f563341ccb4e03c664d
Naman Matoliya <naman@fieldproxy.com>
2025-06-30 08:58:32 +0530

    Navbar section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false)
   return (
     <div className="flex flex-row items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="" />
@@ -50,8 +51,24 @@ const Navbar = () => {
             <img src={assets.cart_icon} className="w-5 cursor-pointer" alt="" />
             <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black leading-4 aspect-square rounded-full text-[8px] text-white">10</p>
         </Link>
-        <img src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden" alt="" />
+        <img onClick={() =>  setVisible(true)} src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden" alt="" />
       </div>
+
+    {/* sidebar menu for smaller screens */}
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' :  'w-0'}`}>
+        <div className="flex flex-col text-gray-600">
+          <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
+            <p>Back</p>
+          </div>
+
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b" to='/'>HOME</NavLink>
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b" to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b" to='/about'>ABOUT</NavLink>
+          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 " to='/contact'>CONTACT</NavLink>
+        </div>
+    </div>
+
     </div>
   );
 };
