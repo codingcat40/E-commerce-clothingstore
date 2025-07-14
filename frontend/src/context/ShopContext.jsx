de03135f6c684daf8c9e8ac64602be49b37a3a16
Naman Matoliya <naman@fieldproxy.com>
2025-07-14 09:33:41 +0530

    rendering product

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,11 +1,18 @@
 import { createContext } from "react";
 import { products } from "../assets/assets";
 
+
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
+
+ const currency = '$';
+ const delivery_fee = 10;
+
+
+
   const value = {
-    products
+    products, currency, delivery_fee
   };
 
   return (
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
 
 const Home = () => {
   return (
-    <Hero />
-  )
-}
+    <div>
+      <Hero />
+      <LatestCollection />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
