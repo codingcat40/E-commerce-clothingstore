f7269246fc8db70bd18e00350656e6a65130d890
Naman Matoliya <naman@fieldproxy.com>
2025-07-22 17:42:20 +0530

    working on cart page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
-  const {setShowSearch} = useContext(ShopContext);
+  const {setShowSearch, getCartCount} = useContext(ShopContext);
   const [visible, setVisible] = useState(false)
   return (
     <div className="flex flex-row items-center justify-between py-5 font-medium">
@@ -51,7 +51,7 @@ const Navbar = () => {
 
         <Link to='/cart' className="relative">
             <img src={assets.cart_icon} className="w-5 cursor-pointer" alt="" />
-            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black leading-4 aspect-square rounded-full text-[8px] text-white">10</p>
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center bg-black leading-4 aspect-square rounded-full text-[8px] text-white">{getCartCount()}</p>
         </Link>
         <img onClick={() =>  setVisible(true)} src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden" alt="" />
       </div>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -12,34 +12,52 @@ const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = async (itemId, size) => {
-
-    if(!size){
-      toast.error('Select product size')
+    if (!size) {
+      toast.error("Select product size");
       return;
     }
 
-    let cartData = structuredClone(cartItems)
-    if(cartData[itemId]){
-        if(cartData[itemId][size]){
-            cartData[itemId][size] += 1
-        }
-        else{
-          cartData[itemId][size] = 1
-        }
-    }
-    else{
-      cartData[itemId] = {}
-      cartData[itemId][size] = 1
+    let cartData = structuredClone(cartItems);
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
     }
 
-    setCartItems(cartData)
+    setCartItems(cartData);
+  };
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return totalCount;
+  };
+
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
 
   };
 
   useEffect(() => {
-      console.log(cartItems)
-  },[cartItems])
-
+    console.log(cartItems);
+  }, [cartItems]);
 
   const value = {
     products,
@@ -51,6 +69,8 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
+    getCartCount,
+    updateQuantity,
   };
 
   return (
